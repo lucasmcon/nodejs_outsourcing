db76a7faa760455ec7c7b543997c954705ba8a89
lucasmcon <lucasmcon@gmail.com>
2020-03-26 21:11:09 -0300

    estruturação

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
     res.send("OLA MUNDO");
